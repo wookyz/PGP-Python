8190853c75c58d5d058326093cca2d8aecd314a0
Wookyz <wookyz@pop-os.localdomain>
2022-12-04 23:47:25 -0300

    Add functions annotations

--- pgp.py
+++ pgp.py
@@ -4,11 +4,11 @@ import math
 
 class PGP:
 
-    def __init__(self, mensagem) -> None:
-        self.generate_key()
+    def __init__(self, mensagem: str) -> None:
+        self.gerarChave()
         self.mensagemCriptografada = self.criptografar(mensagem)
 
-    def generate_key(self):
+    def gerarChave(self) -> None:
         """
         Gera as chaves pública e privada.
         """
@@ -34,10 +34,12 @@ class PGP:
         """
         while True:
             num = random.randint(10, 1000)
+            if num % 2 == 0:
+                num += 1
             if self.__ehPrimo__(num):
                 return num
 
-    def __ehPrimo__(self, n):
+    def __ehPrimo__(self, n: int) -> bool:
         """
         Checa se um número é primo ou não.
         """
@@ -51,14 +53,14 @@ class PGP:
             i += 1
         return True
 
-    def __calculaTotiente__(self, p, q):
+    def __calculaTotiente__(self, p: int, q: int) -> int:
         """
         Calcula a função totiente de n a partir dos primos que geram n.
         """
         totiente = (p - 1) * (q - 1)
         return totiente
 
-    def __calculaCoprimos__(self, totiente):
+    def __calculaCoprimos__(self, totiente: int) -> int:
         """
         Calcula um coprimo de n.
         """
@@ -67,7 +69,7 @@ class PGP:
             if math.gcd(e, totiente) == 1:
                 return e
 
-    def __calculaInverso__(self, e, totiente):
+    def __calculaInverso__(self, e, totiente: int) -> int:
         """
         Calcula o inverso multiplicativo de e.
         """
@@ -77,25 +79,25 @@ class PGP:
                 return d
             d += 1
 
-    def criptografar(self, mensagem):
+    def criptografar(self, mensagem: str) -> list:
         """
         Criptografa a mensagem.
         """
         return [self.criptografarCaractere(char) for char in mensagem]
 
-    def criptografarCaractere(self, char):
+    def criptografarCaractere(self, char: str) -> int:
         """
         Criptografa um caractere.
         """
         return ord(char) ** self.e % self.n
 
-    def descriptografar(self, mensagemCriptografada) -> str:
+    def descriptografar(self, mensagemCriptografada: list) -> str:
         """
         Descriptografa a mensagem.
         """
         return "".join([chr(self.descriptografarCaractere(char)) for char in mensagemCriptografada])
 
-    def descriptografarCaractere(self, char):
+    def descriptografarCaractere(self, char: int) -> int:
         """
         Descriptografa um caractere.
         """
@@ -107,7 +109,7 @@ class PGP:
         """
         return f"\nChave pública: ({self.e}, {self.n})\nChave privada: ({self.d}, {self.n})"
 
-    def exibirDados(self):
+    def exibirDados(self) -> None:
         """
         Exibe os dados da chave.
         """
